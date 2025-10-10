2668a21edf4b5a95072837369b4ceca77c63a1e4
UZAIR <108986693+UZAIR78920@users.noreply.github.com>
2025-10-10 12:31:15 +0530

    Update pandas-test.py

--- pandas-test.py
+++ pandas-test.py
@@ -25,6 +25,25 @@ masked=ma.masked_where(arr==-999,arr)
 print(masked)
 print(masked.mean())
 
+arr=np.array([True,False,True,True])
+print(arr)
+print(arr.dtype)
+
+arr=np.array([10,15,20,25,30])
+mod=arr>20
+print(arr[mod])
+
+arr=np.array([5,10,15,20,25])
+mod=(arr>10) & (arr<25)
+print(mod)
+print(arr[mod])
+
+a=np.array([10,20,30,40,50])
+selected=a[[0,2,4]]
+print(selected)
+
+
+
 # l=[10,20,30,40,50]
 # for i in l :
 #     if not(i>20):
