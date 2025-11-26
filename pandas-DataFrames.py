3a5bdd50d59f8d2c9020741c471a65532ce63282
UZAIR <108986693+UZAIR78920@users.noreply.github.com>
2025-11-26 10:25:44 +0530

    Add examples of pandas DataFrames and Series
    This file contains commented-out examples of using pandas to create DataFrames and Series.

---
+++ pandas-DataFrames.py
@@ -0,0 +1,31 @@
+# import pandas as pd
+# df=pd.DataFrame()
+# print(df)
+
+
+# import pandas as pd
+# s=pd.Series(['a','b','c','d'])
+# df=pd.DataFrame(s)
+# print(df)
+
+
+# import pandas as pd
+# dic={
+#     'name':['Alice','Bob','John'],
+#     'age':[24,23,28],
+#     'salary':[20000,25000,30000]
+# }
+# df=pd.DataFrame(dic)
+# print(df.name)
+# print(df['age'])
+# print(df)
+
+
+# import pandas as pd
+# s=pd.Series([10,20,30,40])
+# df=pd.DataFrame(s)
+# df.columns=['List1']
+# df['List2']=20
+# print(df)
+# df['List3']=df['List1']+df['List2']
+# print(df)
