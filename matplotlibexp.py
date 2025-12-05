1bc78e8a5e2aca9f788760aae0d10cb75774eff9
UZAIR <108986693+UZAIR78920@users.noreply.github.com>
2025-12-05 12:23:04 +0530

    Create matplotlibexp.py

---
+++ matplotlibexp.py
@@ -0,0 +1,18 @@
+# import matplotlib.pyplot as plt
+# x=[1,2,3,4,5]
+# y=[2,3,4,5,6]
+# plt.plot(x,y)
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+# plt.title("Simple Line Plot")
+# plt.show()
+
+
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[5,3,6,2,7]
+plt.scatter(x,y)
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.title("Simple Scatter Plot")
+plt.show()
